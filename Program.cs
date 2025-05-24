a3d9ab200b5662f5a73cd66ba6f71f0eaf3feb38
Kiritmak <iran07ma@gmail.com>
2025-05-24 18:40:13 -0400

    Calculate security profit
    Also Implemented some text functions

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@
 
   public class Penalizado : IPenalizado
   {
-    public Penalizado(string? Formato = "10%")
+    public Penalizado(string? Formato = "10%,Nombre")
     {
       string[] Elementos = Formato.Split(",");
       for(int i = 0; i<Elementos.Length; i++)
@@ -19,21 +19,29 @@
           if (Elementos[i][j] == ' ') continue;
           newElement += Elementos[i][j];
         }
-
         Elementos[i] = newElement;
       }
 
       ValidarFormato(Elementos);
+      CantidadFinal = CalcularPenalizacion();
     }
 
-    double PorcentajePenalizado {  get; set; }
-    public double CantidadFinal { get => CalcularPenalizacion(); }
+    double PorcentajePenalizado { get; set; }
+    public double CantidadFinal
+    {
+      get; private set;
+    }
+    public string Nombre { get; private set; }
 
     public double CalcularPenalizacion()
     {
       //Redondeando al entero mas pequeño
       double CantidadBase = Program.RepartoInicial * PorcentajePenalizado;
       Program.ParseAmmount(ref CantidadBase);
+
+      //Obteniendo ganancia del custodio
+      Program.Custodio += CantidadBase % 10;
+      CantidadBase -= CantidadBase % 10;
 
       return CantidadBase;
     }
@@ -43,6 +51,8 @@
       //Separa el numero seguido de la X en el Formato (X%)
       PorcentajePenalizado = double.Parse(Formato[0].Substring(0, Formato[0].IndexOf('%')));
       PorcentajePenalizado /= 100;
+
+      Nombre = Formato[1];
     }
 
     public override string ToString()
@@ -56,9 +66,9 @@
   {
     static List<Penalizado> Penalizados = new();
     static double RepartoRegular = 0;
-    static double Portero = 0;
     static double CantidadInicial = 0;
     public static double RepartoInicial = 0;
+    public static double Custodio = 0;
     static double RestoDePenalizados = 0;
     static int TotalTrabajadores = 0;
     static int TotalPenalizados = 0;
@@ -73,7 +83,7 @@
       Console.WriteLine("Ingrese la cantidad a repartir, asegurese de separar lugares decimales por puntos (.)\n");
       InputAmmount(out CantidadInicial);
 
-      Console.WriteLine("\nIngrese la cantidad de trabajadores en el turno (Sin incluir al portero)\n");
+      Console.WriteLine("\nIngrese la cantidad de trabajadores en el turno (Sin incluir al custodio)\n");
       InputAmmount(out TotalTrabajadores);
       RepartoInicial = CantidadInicial / TotalTrabajadores;
 
@@ -83,6 +93,7 @@
       Console.WriteLine("\nAhora, ingrese la informacion requerida de cada trabajador penalizado, teniendo en cuenta el siguiente formato");
       Console.WriteLine(FormatoPenalizacion());
 
+      //Recibiendo la data de los penalizados
       for(int i=1; i<=TotalPenalizados; i++)
       {    
         try
@@ -101,10 +112,17 @@
         } 
       }
 
+      //Calculando la ganancia de los no penalizados
       foreach (var p in Penalizados)
         RestoDePenalizados += RepartoInicial-p.CantidadFinal;
-      RepartoRegular = (RestoDePenalizados) / (TotalTrabajadores - TotalPenalizados) + RepartoInicial;
+      RepartoRegular = (RestoDePenalizados) / NoPenalizados() + RepartoInicial;
       ParseAmmount(ref RepartoRegular);
+
+      //Calculando la ganancia del custodio
+      Custodio += (RepartoRegular % 10) * NoPenalizados();
+      RepartoRegular -= RepartoRegular%10;
+
+      DisplayProffits();
     }
 
     static void InputAmmount<T>(out T Number) where T : IParsable<T>
@@ -122,9 +140,11 @@
       return "PorcentajeEnBase100 (X%)\n";
     }
 
-    static void DisplayPenalizados()
+    static void DisplayProffits()
     {
-      string[] Lineas = TextoPenalizados().Split('\n');
+      List<string> Lineas = TextoPenalizados().Split('\n').ToList();
+      Lineas.AddRange(TextoNoPenalizados().Split('\n').ToList());
+      Lineas.AddRange(TextoSobrante().Split('\n').ToList());
       foreach (string Line in Lineas)
         Console.WriteLine(Line);
     }
@@ -132,8 +152,31 @@
     {
       string s = "";
       s += $"{TABBING}Informacion sobre los penalizados\n\n";
-      foreach (var p in Penalizados)
-        s += p.ToString() + "\n\n";
+      for (int i = 1; i <= Penalizados.Count; i++)
+        s += $"{Penalizados[i]}\n\n";
+      s += $"{TABBING}\n";
+      return s;
+    }
+    static string TextoNoPenalizados()
+    {
+      string s = "";
+      s += $"{TABBING}Informacion sobre los no penalizados\n\n";
+      s += $"Ganancia para trabajadores no penalizados: {RepartoRegular}\n";
+      s += $"{TABBING}\n";
+      return s;
+    }
+    static string TextoSobrante()
+    {
+      string s = "";
+      s += $"{TABBING}Informacion sobre dinero restante\n\n";
+      double CantidadTotalRegular = RepartoRegular * NoPenalizados();
+      double CantidadTotalPenalizados = Penalizados.Aggregate((double)0, (acc, p) => acc + p.CantidadFinal);
+      double CantidadTotalReal = Custodio + CantidadTotalPenalizados + CantidadTotalRegular;
+      s += $"Cantidad total a repartir para todos los trabajadores no penalizados: {CantidadTotalRegular}\n";
+      s += $"Cantidad total a repartir para tods los trabajadores penalizados: {CantidadTotalPenalizados}\n";
+      s += $"Cantidad a repartir al custodio: {Custodio}\n";
+      s += $"Cantidad total a repartir: {CantidadTotalReal}\n";
+      s += $"Cantidad restante: {CantidadInicial - CantidadTotalReal}\n";
       s += $"{TABBING}\n";
       return s;
     }
@@ -144,5 +187,13 @@
       Number = Math.Round(Number);
       Number /= 100;
     }
+
+    static int NoPenalizados() => TotalTrabajadores - TotalPenalizados;
   }
 }
+
+/*
+ * 23450.50 
+ * 80%
+ * 50%
+ */
