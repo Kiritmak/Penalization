3fd6d1b3c67b77a3a799a3a517e8169e7b70ad04
Kiritmak <iran07ma@gmail.com>
2025-05-24 14:37:31 -0400

    Create FormatoPenalizacion function
    This function may look simple at first, but it scale easly in the future. Also now the program asks some details of the shift

--- Program.cs
+++ Program.cs
@@ -4,31 +4,46 @@
   {
     static List<double> Penalizados = new List<double>();
     static double Regular = 0;
-    static int Portero = 0;
     static double Sobrante = 0;
     static double CantidadInicial = 0;
-    static int TotalPenalizados = 0;
     static double RepartoInicial = 0;
+    static double Portero = 0;
+    static int TotalTrabajadores = 0;
+    static int TotalPenalizados = 0;
 
     static string TABBING = "\n*****************************************************\n";
 
     public static void Main(string[] args)
     {
-      Console.WriteLine($"Bienvenido a Penalization, Copyright © Kiritmak {TABBING}");
-      Console.WriteLine("Ingrese la cantidad a repartir, asegurese de separar lugares decimales por puntos (.)");
+      Console.WriteLine($"{TABBING} Bienvenido a Penalization, Copyright © Kiritmak {TABBING}");
+
+      Console.WriteLine("Ingrese la cantidad a repartir, asegurese de separar lugares decimales por puntos (.)\n");
       InputAmmount(out CantidadInicial);
-      Console.WriteLine(CantidadInicial);
+
+      Console.WriteLine("\nIngrese la cantidad de trabajadores en el turno\n");
+      InputAmmount(out TotalTrabajadores);
+
+      Console.WriteLine("\nIngrese la cantidad de penalizados en el turno\n");
+      InputAmmount(out TotalPenalizados);
+
+      Console.WriteLine("\nAhora, ingrese la informacion requerida de cada trabajador penalizado, teniendo en cuenta el siguiente formato");
+      Console.WriteLine(FormatoPenalizacion());
+
     }
 
-    static void InputAmmount(out double Number)
+    static void InputAmmount<T>(out T Number) where T : IParsable<T>
     {
-      Number = 0;
       string? Input = Console.ReadLine();
-      while (Input == null || !double.TryParse(Input, out Number)) 
+      while (Input == null || ! T.TryParse(Input, null, out Number) ) 
       { 
         Console.WriteLine("El formato recivido es invalido");
         Input = Console.ReadLine();
       }
+    }
+
+    static string FormatoPenalizacion()
+    {
+      return "PorcentajeEnBase100 (X%)\n";
     }
   }
 }
