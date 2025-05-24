6931d042604075c98345330e7385c0b48c292bdc
Kiritmak <iran07ma@gmail.com>
2025-05-24 14:06:07 -0400

    Create Visual Studio Project

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Penalization
+{
+  public static class Program
+  {
+    public static void Main(string[] args)
+    {
+      Console.WriteLine($"Bienvenido a Penalization, Copyright © Kiritmak");
+    }
+  }
+}
