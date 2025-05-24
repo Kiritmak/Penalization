d170eb4662756749661a551a9e9d4ee84b2f2d48
Kiritmak <iran07ma@gmail.com>
2025-05-24 16:21:19 -0400

    Implement Penalizado class
    Full implementation of Penalizado class

--- Program.cs
+++ Program.cs
@@ -2,34 +2,56 @@
 {
   public interface IPenalizado
   {
-    int CalcularPenalizacion();
+    double CalcularPenalizacion();
+    void ValidarFormato(string[] args);
   }
 
   public class Penalizado : IPenalizado
   {
-    public Penalizado(string? Formato = null)
+    public Penalizado(string? Formato = "  1 0  %")
     {
-      string[] Elementos = Formato.Split(", ");
-      if()
+      string[] Elementos = Formato.Split(",");
+      for(int i = 0; i<Elementos.Length; i++)
+      {
+        string newElement = "";
+        for (int j = 0; j < Elementos[i].Length; j++)
+        {
+          if (Elementos[i][j] == ' ') continue;
+          newElement += Elementos[i][j];
+        }
+
+        Elementos[i] = newElement;
+        Console.WriteLine(Elementos[i]);
+      }
+
+      ValidarFormato(Elementos);
     }
 
     double PorcentajePenalizado {  get; set; }
+    public double CantidadFinal { get => CalcularPenalizacion(); }
 
-    public int CalcularPenalizacion()
+    public double CalcularPenalizacion()
     {
-      throw new NotImplementedException();
+      return Program.RepartoInicial * PorcentajePenalizado;
+    }
+
+    public void ValidarFormato(string[] Formato)
+    {
+      //Separa el numero seguido de la X en el Formato (X%)
+      PorcentajePenalizado = double.Parse(Formato[0].Substring(0, Formato[0].IndexOf('%')));
+      PorcentajePenalizado /= 100;
     }
   }
 
 
   public static class Program
   {
-    static List<double> Penalizados = new List<double>();
+    static List<Penalizado> Penalizados;
     static double Regular = 0;
     static double Sobrante = 0;
     static double CantidadInicial = 0;
-    static double RepartoInicial = 0;
     static double Portero = 0;
+    public static double RepartoInicial = 0;
     static int TotalTrabajadores = 0;
     static int TotalPenalizados = 0;
 
@@ -37,13 +59,15 @@
 
     public static void Main(string[] args)
     {
+      Penalizado penalizado = new Penalizado();
       Console.WriteLine($"{TABBING} Bienvenido a Penalization, Copyright © Kiritmak {TABBING}");
 
       Console.WriteLine("Ingrese la cantidad a repartir, asegurese de separar lugares decimales por puntos (.)\n");
       InputAmmount(out CantidadInicial);
 
-      Console.WriteLine("\nIngrese la cantidad de trabajadores en el turno\n");
+      Console.WriteLine("\nIngrese la cantidad de trabajadores en el turno (Sin incluir al portero)\n");
       InputAmmount(out TotalTrabajadores);
+      RepartoInicial = CantidadInicial / TotalTrabajadores;
 
       Console.WriteLine("\nIngrese la cantidad de penalizados en el turno\n");
       InputAmmount(out TotalPenalizados);
@@ -77,12 +101,6 @@
     static string FormatoPenalizacion()
     {
       return "PorcentajeEnBase100 (X%)\n";
-    }
-
-    public static bool ValidarFormato(string[] Formato)
-    {
-      
-      return true;
     }
   }
 }
